46c6bd68f5372e97065ce2ffb2dfb69a39953fac
Rustam Mehmandarov <mehmandarov@gmail.com>
2018-02-24 11:04:55 +0100

    Time Service: Formatting fix

--- timeservice/src/main/java/no/iot/timeservice/rest/TimeServiceEndpoint.java
+++ timeservice/src/main/java/no/iot/timeservice/rest/TimeServiceEndpoint.java
@@ -3,20 +3,20 @@ package no.iot.timeservice.rest;
 import no.iot.timeservice.supplier.LocalDateTimeNowSupplier;
 
 import javax.inject.Inject;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 @Path("/")
 public class TimeServiceEndpoint {
 
-	@Inject
-	private LocalDateTimeNowSupplier localDateTimeNowSupplier;
+    @Inject
+    private LocalDateTimeNowSupplier localDateTimeNowSupplier;
 
-	@GET
-	@Produces("text/plain")
-	public Response doGet() {
-		return Response.ok(localDateTimeNowSupplier.get()).build();
-	}
+    @GET
+    @Produces("text/plain")
+    public Response doGet() {
+        return Response.ok(localDateTimeNowSupplier.get()).build();
+    }
 }
