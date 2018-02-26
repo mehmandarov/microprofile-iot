4e85273ea0facb3e8e00c7c8b91afc3bfab790ee
Mads Opheim <mads.opheim@gmail.com>
2018-02-26 22:59:17 +0100

    Extracts domain logic from the connector into a single-purpose converter

--- gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/timeConnector/TimeRestConnector.java
+++ gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/timeConnector/TimeRestConnector.java
@@ -2,7 +2,6 @@ package no.cx.iot.philipshueapi.hueController.rest.timeConnector;
 
 import no.cx.iot.philipshueapi.hueController.rest.InputProvider;
 import no.cx.iot.philipshueapi.hueController.rest.hueAPI.HttpConnector;
-import no.cx.iot.philipshueapi.hueController.rest.lights.Brightness;
 import no.cx.iot.philipshueapi.hueController.rest.lights.LightState;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -27,6 +26,9 @@ public class TimeRestConnector implements InputProvider<LocalDateTime> {
     @Inject
     @ConfigProperty(name = "timePath", defaultValue = "timeservice")
     private String path;
+
+    @Inject
+    private TimeToLightStateConverter converter;
 
     private String getFullURL() {
         return "http://" + host + ":" + port +"/" + path;
@@ -55,8 +57,6 @@ public class TimeRestConnector implements InputProvider<LocalDateTime> {
 
     @Override
     public LightState getNewStateForLight(int lightIndex) {
-        LocalDateTime dataForLight = getDataForLight(lightIndex);
-        int newBrightness = dataForLight.getNano() % 255; // TODO: Yes, a bit nonsensical and magical
-        return new LightState(new Brightness(newBrightness), null);
+        return converter.convert(getDataForLight(lightIndex));
     }
 }
