d8dd6b2d8b84779c1852d1d8bfa0416facccc95e
Mads Opheim <mads.opheim@gmail.com>
2018-03-05 20:14:49 +0100

    Gateway: Makes charset configurable, and simplifies brightness with math.max and min

--- gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/lights/Brightness.java
+++ gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/lights/Brightness.java
@@ -1,10 +1,11 @@
 package no.cx.iot.philipshueapi.hueController.rest.lights;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import java.util.logging.Logger;
 
 import javax.inject.Inject;
-import java.util.logging.Logger;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 @EqualsAndHashCode
 public class Brightness {
@@ -17,7 +18,7 @@ public class Brightness {
 
     public Brightness(int brightness) {
         int maxColour = 254;
-        this.brightness = brightness < 0 ? 0 : (brightness > maxColour ? maxColour : brightness);
+        this.brightness = Math.max(0, Math.min(brightness, maxColour));
         if (brightness < 0 || brightness > maxColour) {
             logger.info("Input brightness was " + brightness +", adjusted to " + this.brightness);
         }
