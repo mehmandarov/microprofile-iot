74cb17a058535ea4359384c2cedda7ed3e42a0bc
Mads Opheim <mads.opheim@gmail.com>
2018-03-15 22:01:51 +0100

    Gateway: Added health check for the lights facade, and moved all the health checks into a separate package

--- gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/healthcheck/FacadeHealthChecker.java
+++ gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/healthcheck/FacadeHealthChecker.java
@@ -1,4 +1,4 @@
-package no.cx.iot.philipshueapi.hueController.rest.rest;
+package no.cx.iot.philipshueapi.hueController.rest.healthcheck;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -7,27 +7,26 @@ import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 
-import no.cx.iot.philipshueapi.hueController.rest.weatherConnector.WeatherRestConnector;
+import no.cx.iot.philipshueapi.hueController.rest.hueAPI.PhilipsHueConnector;
 
-@Health
 @ApplicationScoped
-public class WeatherHealthChecker implements HealthCheck {
-
+@Health
+public class FacadeHealthChecker implements HealthCheck {
 
     @Inject
-    private WeatherRestConnector weatherRestConnector;
+    private PhilipsHueConnector hueConnector;
 
     @Override
     public HealthCheckResponse call() {
         try {
-            weatherRestConnector.canConnect();
-            return HealthCheckResponse.named("Weather")
+            hueConnector.getAllLights();
+            return HealthCheckResponse.named("Lights-facade")
                     .up()
                     .build();
         }
         catch (Exception e) {
-            return HealthCheckResponse.named("Weather")
-                    .withData("Weather", e.getMessage())
+            return HealthCheckResponse.named("Lights-facade")
+                    .withData("Lights", e.getMessage())
                     .down()
                     .build();
         }
--- gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/healthcheck/WeatherHealthChecker.java
+++ gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/healthcheck/WeatherHealthChecker.java
@@ -1,4 +1,4 @@
-package no.cx.iot.philipshueapi.hueController.rest.rest;
+package no.cx.iot.philipshueapi.hueController.rest.healthcheck;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -7,26 +7,27 @@ import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 
-import no.cx.iot.philipshueapi.hueController.rest.timeConnector.TimeRestConnector;
+import no.cx.iot.philipshueapi.hueController.rest.weatherConnector.WeatherRestConnector;
 
 @Health
 @ApplicationScoped
-public class TimeHealthChecker implements HealthCheck {
+public class WeatherHealthChecker implements HealthCheck {
+
 
     @Inject
-    private TimeRestConnector timeRestConnector;
+    private WeatherRestConnector weatherRestConnector;
 
     @Override
     public HealthCheckResponse call() {
         try {
-            timeRestConnector.canConnect();
-            return HealthCheckResponse.named("Time")
+            weatherRestConnector.canConnect();
+            return HealthCheckResponse.named("Weather")
                     .up()
                     .build();
         }
         catch (Exception e) {
-            return HealthCheckResponse.named("Time")
-                    .withData("Time", e.getMessage())
+            return HealthCheckResponse.named("Weather")
+                    .withData("Weather", e.getMessage())
                     .down()
                     .build();
         }
