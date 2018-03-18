b3fb09c8bcb9964abc872512479e0d6d10da759f
Mads Opheim <mads.opheim@gmail.com>
2018-03-18 13:41:21 +0100

    Facade: Extracts light colour into config properties

--- gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/weatherConnector/WeatherToLightStateConverter.java
+++ gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/weatherConnector/WeatherToLightStateConverter.java
@@ -2,6 +2,10 @@ package no.cx.iot.philipshueapi.hueController.rest.weatherConnector;
 
 import java.awt.Color;
 
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import no.cx.iot.philipshueapi.hueController.rest.Converter;
 import no.cx.iot.philipshueapi.hueController.rest.InputSource;
 import no.cx.iot.philipshueapi.hueController.rest.lights.Brightness;
@@ -9,9 +13,21 @@ import no.cx.iot.philipshueapi.hueController.rest.lights.LightState;
 
 public class WeatherToLightStateConverter implements Converter<Weather> {
 
+    @Inject
+    @ConfigProperty(name = "r", defaultValue = "0")
+    private int r;
+
+    @Inject
+    @ConfigProperty(name = "g", defaultValue = "0")
+    private int g;
+
+    @Inject
+    @ConfigProperty(name = "b", defaultValue = "255")
+    private int b;
+
     @Override
     public LightState convert(int lightIndex, Weather temperature) {
-        int hue = Color.MAGENTA.getRGB();
+        int hue = new Color(r, g, b).getRGB();
         return new LightState(lightIndex, InputSource.WEATHER, new Brightness(getBrightnessStrength(temperature)), hue);
     }
 
