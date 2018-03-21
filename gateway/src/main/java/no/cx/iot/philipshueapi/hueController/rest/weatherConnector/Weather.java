4ade11172cde1d2189ba5abb0085d83c3ac2794f
Mads Opheim <mads.opheim@gmail.com>
2018-03-22 01:50:41 +0200

    Gateway: Some more tests

--- gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/weatherConnector/Weather.java
+++ gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/weatherConnector/Weather.java
@@ -1,12 +1,10 @@
 package no.cx.iot.philipshueapi.hueController.rest.weatherConnector;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class Weather {
     private String temperature;
 
