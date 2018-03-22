94c9c4a15c6257b35fe681d1e5e60cf8ac0bc982
Mads Opheim <mads.opheim@gmail.com>
2018-03-22 19:38:35 +0200

    Facade: Extracted bridge connection and selection into a separate class

---
+++ hueAPI/src/main/java/no/cx/iot/philipshueapi/hueAPI/BridgeSelector.java
@@ -0,0 +1,51 @@
+package no.cx.iot.philipshueapi.hueAPI;
+
+import java.util.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import lombok.Getter;
+import no.cx.iot.philipshueapi.hueAPI.logic.Bridge;
+import no.cx.iot.philipshueapi.hueAPI.logic.SDKBridge;
+import no.cx.iot.philipshueapi.hueAPI.sdk.SDKFacade;
+
+@ApplicationScoped
+class BridgeSelector {
+
+    @Inject
+    @ConfigProperty(name = "useRealBridge", defaultValue = "false")
+    @Getter
+    private boolean useRealBridge;
+
+    @Inject
+    private SDKFacade sdk;
+
+    @Inject
+    private Logger logger;
+
+    void waitUntilBridgeIsSelected() {
+        int counter = 0;
+        if (!useRealBridge) {
+            return;
+        }
+        while (sdk.getSelectedBridge() == null) {
+            try {
+                logger.info("Waiting for bridge selection");
+                Thread.sleep(400);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (counter++ > 50) {
+                throw new HueAPIException("Waited too long for bridgeselection");
+            }
+        }
+    }
+
+    Bridge getBridge() {
+        return useRealBridge ? new SDKBridge(sdk.getSelectedBridge()) : new DummyBridge();
+    }
+}
