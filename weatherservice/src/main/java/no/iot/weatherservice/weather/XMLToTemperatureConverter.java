419c74a4674272cf82ed6b673d99282272e6e55c
Mads Opheim <mads.opheim@gmail.com>
2018-03-27 01:23:05 +0300

    18, weather service: Added example of a weather forecast without observations, and made the parsing handle it

--- weatherservice/src/main/java/no/iot/weatherservice/weather/XMLToTemperatureConverter.java
+++ weatherservice/src/main/java/no/iot/weatherservice/weather/XMLToTemperatureConverter.java
@@ -1,20 +1,51 @@
 package no.iot.weatherservice.weather;
 
+import java.util.logging.Logger;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 
 @RequestScoped
 public class XMLToTemperatureConverter {
 
+    @Inject
+    private Logger logger;
+
     // Yes, this is obscure. But it works. At least atm. Should be improved.
     public String convert(String xml) {
-        String[] observations = xml.split("<observations>", 2);
-        if (observations.length >= 1) {
-            if (!observations[0].startsWith("ERROR")) {
-                String[] observation = observations[1].split("<temperature unit=\"celsius\" value=", 2);
-                String[] onlyTemperature = observation[1].split("time", 2);
-                return onlyTemperature[0].replaceAll("\"", "").trim();
+        try {
+            String country = getCountry(xml);
+
+            if ("Norway".equals(country)) {
+                String[] observations = xml.split("<observations>", 2);
+                if (observations.length > 1) {
+                    if (!observations[0].startsWith("ERROR")) {
+                        return getTemperature(observations[1]);
+                    }
+                }
+            } else if (xml.contains("temperature")) {
+                String temperature = getTemperature(xml);
+                if (!temperature.contains("ERROR")) {
+                    return temperature;
+                }
             }
         }
+        catch (RuntimeException e) {
+            logger.warning(e.getMessage());
+        }
         return "ERROR";
+    }
+
+    private String getCountry(String xml) {
+        return xml.split("country>", 3)[1].replace("</", "");
+    }
+
+    private String getTemperature(String observation1) {
+        String[] observation = observation1.split("<temperature unit=\"celsius\" value=", 2);
+        String[] onlyTemperature = observation[1].split("time", 2);
+        String[] x = onlyTemperature[0].split("/>", 2);
+        return x[0].
+                replaceAll("\"", "")
+                .trim();
     }
 }
--- weatherservice/test/main/java/no/iot/weatherservice/weather/XMLToTemperatureConverterTest.java
+++ weatherservice/test/main/java/no/iot/weatherservice/weather/XMLToTemperatureConverterTest.java
@@ -1,22 +1,26 @@
 package no.iot.weatherservice.weather;
 
 
-import com.google.common.io.Files;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.runners.MockitoJUnitRunner;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.common.io.Files;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -25,16 +29,29 @@ public class XMLToTemperatureConverterTest {
     @InjectMocks
     private XMLToTemperatureConverter converter;
 
+    @Mock
+    private Logger logger;
+
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void parsesTemperatureFromExampleYrFileCorrectly() throws IOException {
-        String entireXML = readFile().stream().collect(Collectors.joining("\n"));
+        String entireXML = readFile("exampleOfYrResponse.txt").stream().collect(Collectors.joining("\n"));
 
         String temperature = converter.convert(entireXML);
 
         assertThat(temperature, is("-2.7"));
+    }
+
+    @Test
+    public void parsesTemperatureFromChisinauResultsCorrectly() throws IOException {
+        String entireXML = readFile("chisinau.xml").stream().collect(Collectors.joining("\n"));
+
+        String temperature = converter.convert(entireXML);
+
+        assertEquals(temperature, "2");
+
     }
 
     @Test
@@ -44,13 +61,13 @@ public class XMLToTemperatureConverterTest {
     }
 
     @Test
-    public void throwsExceptionIfInputIsJustRubbish() {
-        expectedException.expect(RuntimeException.class);
-        converter.convert("trall");
+    public void returnsErrorIfInputIsJustRubbish() {
+        String returnedMessage = converter.convert("trall");
+        assertThat(returnedMessage, is("ERROR"));
     }
 
-    private List<String> readFile() throws IOException {
-        File file = Paths.get(".").resolve("src/main").resolve("resources").resolve("exampleOfYrResponse.txt").toAbsolutePath().toFile();
+    private List<String> readFile(String filename) throws IOException {
+        File file = Paths.get(".").resolve("src/main").resolve("resources").resolve(filename).toAbsolutePath().toFile();
         return Files.readLines(file, Charset.defaultCharset());
     }
 
