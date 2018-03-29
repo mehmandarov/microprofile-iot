66afed62502d370af272598b0368bd8d651b0092
Mads Opheim <mads.opheim@gmail.com>
2018-03-29 16:04:30 +0300

    Weather service: Extracted the generic error message text into a config property

--- weatherservice/src/main/java/no/iot/weatherservice/WeatherServiceEndpoint.java
+++ weatherservice/src/main/java/no/iot/weatherservice/WeatherServiceEndpoint.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
@@ -16,21 +17,23 @@ public class WeatherServiceEndpoint {
     @Inject
     private LocalWeatherSupplier localWeatherSupplier;
 
+    @Inject
+    @ConfigProperty(name = "errorMessage")
+    private String errorMessage;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(summary = "Get the current temperature")
     @APIResponse(name = "WeatherDTO")
     public Response getWeather() {
         try {
-            return Response.ok(localWeatherSupplier.get()).build();
+            return Response
+                    .ok(localWeatherSupplier.get())
+                    .build();
         } catch (NumberFormatException ex) {
-            return createErrorMessage();
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE)
+                    .entity(errorMessage)
+                    .build();
         }
-    }
-
-    private Response createErrorMessage() {
-        return Response.status(Response.Status.SERVICE_UNAVAILABLE)
-                .entity("We were not able to get the temperature for this location right now.")
-                .build();
     }
 }
