945e904f985446ad782a132e9c8670318adf940b
Mads Opheim <mads.opheim@gmail.com>
2018-03-29 16:19:15 +0300

    Time service: Éxtracted generic error message text into property

--- timeservice/src/main/java/no/iot/timeservice/TimeServiceEndpoint.java
+++ timeservice/src/main/java/no/iot/timeservice/TimeServiceEndpoint.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 
 import no.iot.timeservice.supplier.ZonedDateTimeNowSupplier;
@@ -24,17 +25,22 @@ public class TimeServiceEndpoint {
     @Inject
     private Logger logger;
 
+    @Inject
+    @ConfigProperty(name = "errorMessage")
+    private String errorMessage;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(summary = "Gets the current time", description = "The format is a wrapped ZonedDateTime")
-    public Response doGet() {
+    public Response getCurrentTime() {
         try {
             logger.info("Invoking timeservice.");
-            return Response.ok(dateTimeNowSupplier.get()).build();
+            return Response
+                    .ok(dateTimeNowSupplier.get())
+                    .build();
         } catch (NumberFormatException ex) {
             return Response.status(Response.Status.SERVICE_UNAVAILABLE)
-                    .entity("We were not able to get the current time at this moment.")
+                    .entity(errorMessage)
                     .build();
         }
     }
