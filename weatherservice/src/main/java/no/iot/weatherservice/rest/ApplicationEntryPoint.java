5018836f9757194c86af934500d46b3485ebd5dc
Mads Opheim <mads.opheim@gmail.com>
2018-03-29 15:05:16 +0300

    Weather service: Minor refactorings

--- weatherservice/src/main/java/no/iot/weatherservice/rest/ApplicationEntryPoint.java
+++ weatherservice/src/main/java/no/iot/weatherservice/rest/ApplicationEntryPoint.java
@@ -1,17 +1,16 @@
 package no.iot.weatherservice.rest;
 
+import java.util.Collections;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
 @ApplicationPath("/weatherservice")
 public class ApplicationEntryPoint extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
-        Set<Class<?>> classes = new HashSet<>();
-        classes.add(WeatherServiceEndpoint.class);
-        return classes;
+        return Collections.singleton(WeatherServiceEndpoint.class);
     }
 }
--- weatherservice/src/main/java/no/iot/weatherservice/rest/WeatherDTO.java
+++ weatherservice/src/main/java/no/iot/weatherservice/rest/WeatherDTO.java
@@ -5,8 +5,4 @@ import lombok.Data;
 @Data
 public class WeatherDTO {
     private final String temperature;
-
-    public WeatherDTO(String temperature) {
-        this.temperature = temperature;
-    }
 }
