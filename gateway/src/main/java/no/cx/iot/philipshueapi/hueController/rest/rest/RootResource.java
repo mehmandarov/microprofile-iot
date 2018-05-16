27d4807909f92b1ddf794134f4621fe976238412
Mads Opheim <mads.opheim@gmail.com>
2018-05-16 19:15:27 +0200

    Gateway: Updated imports for the moved files from previous commit

--- gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/rest/RootResource.java
+++ gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/rest/RootResource.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import no.cx.iot.philipshueapi.hueController.rest.LightStateController;
+import no.cx.iot.philipshueapi.hueController.rest.lightController.LightStateController;
 import no.cx.iot.philipshueapi.hueController.rest.timeConnector.TimeRestConnector;
 import no.cx.iot.philipshueapi.hueController.rest.weatherConnector.WeatherRestConnector;
 
