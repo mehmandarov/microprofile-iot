7c038fa0f8c0e90638e85f01d6d110e96a097281
Mads Opheim <mads.opheim@gmail.com>
2018-05-23 20:40:33 +0200

    Gateway: Made kubernetes yaml file

--- gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/rest/ApplicationEntryPoint.java
+++ gateway/src/main/java/no/cx/iot/philipshueapi/hueController/rest/rest/ApplicationEntryPoint.java
@@ -1,17 +1,19 @@
 package no.cx.iot.philipshueapi.hueController.rest.rest;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
-import com.google.common.collect.Sets;
 
 @ApplicationPath("/hue")
 public class ApplicationEntryPoint extends Application {
 
 	@Override
 	public Set<Class<?>> getClasses() {
-		return Sets.newHashSet(RootResource.class, VerifyResource.class);
+		Set<Class<?>> classes = new HashSet<>();
+		classes.add(RootResource.class);
+		classes.add(VerifyResource.class);
+		return classes;
 	}
 }
