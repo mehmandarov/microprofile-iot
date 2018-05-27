94b797c4610ff04268b294dbc1531540086133ce
Mads Opheim <mads.opheim@gmail.com>
2018-05-27 11:39:17 +0200

    Gateway, weatherservice: Replaces the ThrowingSupplier with the existing Callable class

--- weatherservice/src/main/java/no/iot/weatherservice/utils/general/ExceptionWrapper.java
+++ weatherservice/src/main/java/no/iot/weatherservice/utils/general/ExceptionWrapper.java
@@ -1,7 +1,13 @@
 package no.iot.weatherservice.utils.general;
 
+import java.util.concurrent.Callable;
+
 public class ExceptionWrapper {
-    public static <T> T wrapExceptions(ThrowingSupplier<T> throwingSupplier) {
-        return throwingSupplier.get();
+    public static <T> T wrapExceptions(Callable<T> throwingSupplier) {
+        try {
+            return throwingSupplier.call();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
