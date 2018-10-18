91b600850e307d88d3f7e9870a3763cce0e8dfde
Mads Opheim <mads.opheim@gmail.com>
2018-10-18 21:08:38 +0200

    Time: minor tweaks after changing appserver

--- timeservice/src/main/java/no/cx/iot/timeservice/ApplicationEntryPoint.java
+++ timeservice/src/main/java/no/cx/iot/timeservice/ApplicationEntryPoint.java
@@ -1,5 +1,8 @@
 package no.cx.iot.timeservice;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+
 import java.util.Collections;
 import java.util.Set;
 
@@ -7,6 +10,10 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/timeservice")
+@OpenAPIDefinition(info = @Info(
+        title = "Timeservice",
+        version = "1.0"
+))
 public class ApplicationEntryPoint extends Application {
 
     @Override
--- timeservice/src/main/java/no/cx/iot/timeservice/TimeServiceEndpoint.java
+++ timeservice/src/main/java/no/cx/iot/timeservice/TimeServiceEndpoint.java
@@ -1,5 +1,6 @@
 package no.cx.iot.timeservice;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -7,9 +8,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 
-@Path("/")
+@Path("")
+@Schema(name = "Timeservice")
+@ApplicationScoped
 public class TimeServiceEndpoint {
 
     @SuppressWarnings("unused")
@@ -18,6 +23,7 @@ public class TimeServiceEndpoint {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @APIResponse(description = "The current time")
     @Operation(summary = "Gets the current time", description = "The format is a wrapped ZonedDateTime")
     public TimeDTO getCurrentTime() {
         return dateTimeNowSupplier.get();
