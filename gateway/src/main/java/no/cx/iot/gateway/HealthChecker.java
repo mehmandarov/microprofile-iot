0cb0320f97b70f7ed6ef5a0e236432b3bfea02c5
Mads Opheim <mads.opheim@gmail.com>
2018-11-05 22:36:07 +0100

    Gateway: Simplified (and stupidified) health check, and removed the dependency to httpclient

--- gateway/src/main/java/no/cx/iot/gateway/HealthChecker.java
+++ gateway/src/main/java/no/cx/iot/gateway/HealthChecker.java
@@ -1,11 +1,9 @@
 package no.cx.iot.gateway;
 
+import java.util.Random;
+
 import javax.enterprise.context.ApplicationScoped;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
@@ -31,12 +29,6 @@ public class HealthChecker implements HealthCheck {
     }
 
     private boolean canConnect() {
-        try {
-            HttpUriRequest request = new HttpGet("http://localhost:9080/hue/verify");
-            CloseableHttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-            return httpResponse.getStatusLine().getStatusCode() == 200;
-        } catch (Exception e) {
-            return false;
-        }
+        return new Random().nextBoolean();
     }
 }
