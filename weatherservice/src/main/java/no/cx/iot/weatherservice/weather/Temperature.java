153f4f797eeb2ceadcca913bf43a1acd047aa8e7
Mads Opheim <mads.opheim@gmail.com>
2018-11-05 22:13:53 +0100

    Weather: empty constructor in the temperature class, in order to make it deserializable

--- weatherservice/src/main/java/no/cx/iot/weatherservice/weather/Temperature.java
+++ weatherservice/src/main/java/no/cx/iot/weatherservice/weather/Temperature.java
@@ -2,15 +2,22 @@ package no.cx.iot.weatherservice.weather;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @EqualsAndHashCode
 @ToString
+@NoArgsConstructor
 public class Temperature {
-    private final String temperature;
+
+    private String temperature;
 
     public Temperature(String temperature) {
+        setTemperature(temperature);
+    }
+
+    public void setTemperature(String temperature) {
         validate(temperature);
         this.temperature = temperature;
     }
