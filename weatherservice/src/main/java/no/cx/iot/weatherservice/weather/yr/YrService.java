ac53c587cc3d196a39e4ee853a3606e8eec73302
Mads Opheim <mads.opheim@gmail.com>
2018-11-18 13:22:53 +0100

    Gateway, weather: Using more MP-fault tolerance

--- weatherservice/src/main/java/no/cx/iot/weatherservice/weather/yr/YrService.java
+++ weatherservice/src/main/java/no/cx/iot/weatherservice/weather/yr/YrService.java
@@ -4,11 +4,16 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Retry;
+
 @Path("/place")
 public interface YrService {
 
     @GET
     @Path("/{country}/{cityPart}/forecast.xml")
+    @Retry(maxRetries = 2)
+    @CircuitBreaker
     String getWeather(
             @PathParam("country") String country,
             @PathParam("cityPart") String cityPart
