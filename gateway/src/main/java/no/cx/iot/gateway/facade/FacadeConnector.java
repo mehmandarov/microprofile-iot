cf5a91e4a2a688e795cc83437225ba68be0d7b6a
Mads Opheim <mads.opheim@gmail.com>
2018-11-18 12:12:33 +0100

    Gateway: Simplified facade connection classes

--- gateway/src/main/java/no/cx/iot/gateway/facade/FacadeConnector.java
+++ gateway/src/main/java/no/cx/iot/gateway/facade/FacadeConnector.java
@@ -1,10 +1,12 @@
 package no.cx.iot.gateway.facade;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import no.cx.iot.gateway.infrastructure.Connector;
@@ -15,8 +17,12 @@ import no.cx.iot.gateway.lights.LightState;
 public class FacadeConnector implements Connector {
 
     @Inject
-    @SuppressWarnings("unused")
-    private FacadeURL facadeURL;
+    @ConfigProperty(name="facadehost", defaultValue = "localhost")
+    private String host;
+
+    @Inject
+    @ConfigProperty(name="port", defaultValue = "8083")
+    private String port;
 
     @Inject
     @RestClient
@@ -30,12 +36,16 @@ public class FacadeConnector implements Connector {
         return facadeEndpoint.switchStateOfLight(
                 newLightState.getLightIndex(),
                 newLightState.getBrightnessInt(),
-                facadeURL.getColor(newLightState)
+                getColor(newLightState)
         );
     }
 
     @Override
     public void testConnection() throws IOException {
         getNumberOfLights();
+    }
+
+    private Integer getColor(LightState newLightState) {
+        return Optional.ofNullable(newLightState.getHueInt()).orElse(0);
     }
 }
--- gateway/test/main/java/no/cx/iot/gateway/facade/FacadeConnectorTest.java
+++ gateway/test/main/java/no/cx/iot/gateway/facade/FacadeConnectorTest.java
@@ -4,13 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import no.cx.iot.gateway.lights.Brightness;
 import no.cx.iot.gateway.lights.LightState;
 
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -19,15 +17,11 @@ public class FacadeConnectorTest {
     @Mock
     private FacadeEndpoint facadeEndpoint;
 
-    @Spy
-    private FacadeURL facadeURL;
-
     @InjectMocks
     private FacadeConnector facadeConnector;
 
     @Test
     public void hueURLIsComposedProperly() {
-        doReturn("localhozt/").when(facadeURL).getFullURL();
         LightState newLightState = new LightState();
         newLightState.setLightIndex(1);
         newLightState.setBrightness(new Brightness(2));
