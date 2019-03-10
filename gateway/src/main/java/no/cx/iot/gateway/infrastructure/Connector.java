d505c6b93cb4296f19adb08b3289935ecc9a18fa
Mads Opheim <mads.opheim@gmail.com>
2019-03-10 18:34:33 +0100

    Gateway: Can't use timed on a default interface method implemented by several classes, it seems

--- gateway/src/main/java/no/cx/iot/gateway/infrastructure/Connector.java
+++ gateway/src/main/java/no/cx/iot/gateway/infrastructure/Connector.java
@@ -2,11 +2,8 @@ package no.cx.iot.gateway.infrastructure;
 
 import java.io.IOException;
 
-import org.eclipse.microprofile.metrics.annotation.Timed;
-
 public interface Connector {
 
-    @Timed(absolute = true, name = "canConnect", description = "Can connect-checks")
     default boolean canConnect() {
         try {
             testConnection();
