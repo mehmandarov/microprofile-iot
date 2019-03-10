f8128aef97732ef2ce32b0d5044cdb5e5223d83a
Mads Opheim <mads.opheim@gmail.com>
2019-03-10 17:46:00 +0100

    Time: Made TimeDTO serializable for Jackson again

--- timeservice/src/main/java/no/cx/iot/timeservice/TimeDTO.java
+++ timeservice/src/main/java/no/cx/iot/timeservice/TimeDTO.java
@@ -3,7 +3,18 @@ package no.cx.iot.timeservice;
 import java.time.ZonedDateTime;
 
 public class TimeDTO {
-    private final String timeRepresentation;
+    private String timeRepresentation;
+
+    public TimeDTO() {
+    }
+
+    public String getTimeRepresentation() {
+        return timeRepresentation;
+    }
+
+    public void setTimeRepresentation(String timeRepresentation) {
+        this.timeRepresentation = timeRepresentation;
+    }
 
     public TimeDTO(ZonedDateTime zonedDateTime) {
         timeRepresentation = zonedDateTime.toString();
