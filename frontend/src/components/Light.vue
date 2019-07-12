93b1de8ce3b629824a88ef7e9295ad626922c32b
Mads Opheim <mads.opheim@gmail.com>
2019-07-12 23:36:45 +0200

    Rough first version of frontend

---
+++ frontend/src/components/Light.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="light">
+    <h1>Light {{ number }}: {{ status }}</h1>
+    <img src="@/assets/logo.png" :alt="status" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Light extends Vue {
+  @Prop() private status!: string;
+  @Prop() private number!: number;
+
+  public setNumber(newNumber: number) {
+      this.number = newNumber;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+    img {
+        width: 5rem;
+        height: 5rem;
+    }
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="lights">
+      <div v-for="light in lights" :key="light.number">
+        <Light status="Off" :number="light.number"/>
+      </div>
+    </div>
+    <div id="buttons">
+      <button type="button" v-on:click="resetLights">Reset lights</button>
+      <button type="button" v-on:click="setLights">Set lights</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Light from '@/components/Light.vue';
 
-@Component({
-  components: {
-    HelloWorld,
+export default Vue.extend({
+  data: () => ({
+    lights: [] as Light[],
+  }),
+  mounted() {
+    this.createLight(0);
+    this.createLight(1);
+    this.createLight(2);
+    this.createLight(3);
   },
-})
-export default class Home extends Vue {}
+  methods: {
+    createLight: function(index: number) {
+      const light = new Light();
+      light.setNumber(index);
+      this.lights.push(light);
+    },
+    resetLights: () => {
+      // todo
+    },
+    setLights: () => {
+      // todo
+    },
+  },
+  components: {
+      Light,
+    },
+});
 </script>
+
+<style scoped lang="scss">
+    div.light {
+      float: left;
+      min-width: 15%;
+      margin: 10px;
+      border: 1px solid black;
+      padding: 10px;
+    };
+    
+    div.lights {
+      border: 1px solid blue;
+      margin: 0 auto;
+      width: 70%;
+    };
+
+    div#buttons {
+      clear: both;
+      min-width: 200px;
+      margin: 0 auto;
+    };
+
+    button {
+      min-height: 200px;
+      height: 300px;
+      margin: 5px;
+      width: 30%;
+    };
+</style>
